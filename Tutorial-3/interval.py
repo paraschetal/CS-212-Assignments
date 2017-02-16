4e15bc6c5dd8978ef4013711c558bc7c43b3d71b
Paras Chetal <paras.chetal@gmail.com>
2017-02-16 23:39:16 +0530

    Add delete implementation

--- Tutorial-3/interval.py
+++ Tutorial-3/interval.py
@@ -47,19 +47,114 @@ class IntervalTree:
                 else:
                     self.root.rightSubTree.insert(node)
                     
-'''    
+ 
     def delete(self, interval):
         if self.root.interval == interval:
 
-            if self.root.rightSubTree != None:
-                temp = self.root.rightSubTree.root
+            whichChild = ''
+            if self.root.parent.rightSubTree == self:
+                whichChild = 'right'
                 
-        return
+            elif self.root.parent.leftSubTree == self:
+                whichChild = 'left'
+                
+
+                
+            if not self.root.rightSubTree and not self.root.leftSubTree: #If the node has no child
+                if whichChild == 'left':
+                    self.root.parent.leftSubTree = None
+                else:
+                    self.root.parent.rightSubTree = None
+
+                    
+            elif not self.root.rightSubTree or not self.root.leftSubTree: #If the node has only one child
+                
+                if self.root.rightSubTree != None:
+                    if whichChild == 'left':
+                        self.root.parent.leftSubTree = self.root.rightSubTree
+                    else:
+                        self.root.parent.rightSubTree = self.root.rightSubTree
+                        
+                        
+                else:
+                    if whichChild == 'left':
+                        self.root.parent.leftSubTree = self.root.leftSubTree
+                    else:
+                        self.root.parent.rightSubTree = self.root.leftSubTree
+                        
+            
+            else: #If the node has both children
+               
+                leftmostSubChildOfTheRightNode = self.root.rightSubTree
+                while(leftmostSubChildOfTheRightNode.root.leftSubTree!=None):
+                    leftmostSubChildOfTheRightNode = leftmostSubChildOfTheRightNode.root.leftSubTree
+                    
+                lefttemp = self.root.leftSubTree
+                righttemp = self.root.rightSubTree
+                
+                if whichChild == 'left':
+                    
+                    self.root.parent.leftSubTree.root = leftmostSubChildOfTheRightNode.root
+                    self.root.leftSubTree = lefttemp
+                    if righttemp.root.leftSubTree == None:
+                        self.root.rightSubTree = None
+                    else:
+                        self.root.rightSubTree = righttemp
+                   
+                    if not lefttemp and not righttemp:
+                        pass
+                    elif not lefttemp and not (not righttemp):
+                        self.root.maxInSubTree = righttemp.root.maxInSubTree
+                    elif not (not lefttemp) and not righttemp:
+                        self.root.maxInSubTree = lefttemp.root.maxInSubTree
+                    else:
+                        pass
+                        
+
+                else:
+                    self.root.parent.rightSubTree.root = leftmostSubChildOfTheRightNode.root
+                    self.root.leftSubTree = self.root.leftSubTree
+                    if righttemp.root.leftSubTree == None:
+                        self.root.rightSubTree = None
+                    else:
+                        self.root.rightSubTree = self.root.rightSubTree
+
+                    if not lefttemp and not righttemp:
+                        pass
+                    elif not lefttemp and not (not righttemp):
+                        self.root.maxInSubTree = righttemp.root.maxInSubTree
+                    elif not (not lefttemp) and not righttemp:
+                        self.root.maxInSubTree = lefttemp.root.maxInSubTree
+                    else:
+                        pass
+
+                
+                self.delete(leftmostSubChildOfTheRightNode.root.interval)
+
+            
+        else:
+            
+            if min(i for i in interval) < self.root.low:
+                if not self.root.leftSubTree:
+                    print "1. No such interval found."
+                else:
+                    self.root.leftSubTree.delete(interval)
+                    
+            elif min(i for i in interval) > self.root.low:
+                if not self.root.rightSubTree:
+                    print "2. No such interval found."
+                else:
+                    self.root.rightSubTree.delete(interval)
+                    
+            else:
+                print "3. No such interval found."
+                
+
 
 
     def search(self, interval):
         return
-'''    
+   
 
 if __name__ == '__main__':
 
@@ -70,6 +165,7 @@ if __name__ == '__main__':
 
     for data in sampleData[1:]:
         tree.insert(IntervalNode(data))
+
 '''
     print tree.root.__dict__
 
@@ -80,4 +176,16 @@ if __name__ == '__main__':
     print tree.root.leftSubTree.root.rightSubTree.root.__dict__
 
     print tree.root.rightSubTree.root.rightSubTree.root.__dict__        
+'''
+    
+    tree.delete({10,30})
+    
+'''   
+    print tree.root.__dict__
+
+    print tree.root.leftSubTree.root.__dict__
+    print tree.root.rightSubTree.root.__dict__
+
+    print tree.root.leftSubTree.root.leftSubTree.root.__dict__
+    print tree.root.rightSubTree.root.rightSubTree.root.__dict__
 '''
