9f552d57c3f9587094a677aebefd8b0a0c2ea239
Paras Chetal <paras.chetal@gmail.com>
2017-02-16 20:45:50 +0530

    Add parent for each node

--- Tutorial-3/interval.py
+++ Tutorial-3/interval.py
@@ -16,6 +16,10 @@ class IntervalNode:
         self.low = min(i for i in interval)
         self.leftSubTree = None
         self.rightSubTree = None
+        self.parent = None
+
+    def setParent(self, parent):
+        self.parent = parent
 
 
 class IntervalTree:
@@ -32,40 +36,48 @@ class IntervalTree:
             if node.low < self.root.low:
                 if self.root.leftSubTree == None:
                     self.root.leftSubTree = IntervalTree(node)
+                    self.root.leftSubTree.root.setParent(self.root)
                 else:
                     self.root.leftSubTree.insert(node)
 
             else:
                 if self.root.rightSubTree == None:
                     self.root.rightSubTree = IntervalTree(node)
+                    self.root.rightSubTree.root.setParent(self.root)
                 else:
                     self.root.rightSubTree.insert(node)
-        #print self.root.__dict__
-    
-    def delete(self, root, interval):
+                    
+'''    
+    def delete(self, interval):
+        if self.root.interval == interval:
+
+            if self.root.rightSubTree != None:
+                temp = self.root.rightSubTree.root
+                
         return
-    
+
+
     def search(self, interval):
         return
-    
+'''    
 
 if __name__ == '__main__':
 
     sampleData = [{15, 20}, {10, 30}, {17, 19},{5, 20}, {12, 15}, {30, 40}]
     root = IntervalNode(sampleData[0])
-    print root.__dict__
 
     tree = IntervalTree(root)
 
     for data in sampleData[1:]:
         tree.insert(IntervalNode(data))
+'''
+    print tree.root.__dict__
 
-    #print tree.root.__dict__
+    print tree.root.leftSubTree.root.__dict__
+    print tree.root.rightSubTree.root.__dict__
 
-    #print tree.root.leftSubTree.root.__dict__
-    #print tree.root.rightSubTree.root.__dict__
+    print tree.root.leftSubTree.root.leftSubTree.root.__dict__
+    print tree.root.leftSubTree.root.rightSubTree.root.__dict__
 
-    #print tree.root.leftSubTree.root.leftSubTree.root.__dict__
-    #print tree.root.leftSubTree.root.rightSubTree.root.__dict__
-
-    #print tree.root.rightSubTree.root.rightSubTree.root.__dict__        
+    print tree.root.rightSubTree.root.rightSubTree.root.__dict__        
+'''
